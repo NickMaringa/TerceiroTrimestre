39ae4c2e5bd1716042b7542692d055369eacc330
NickMaringa <106190059+NickMaringa@users.noreply.github.com>
2022-10-11 16:53:26 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -29,9 +29,9 @@ function quadrado (){
   }
 }
 function total (){
-  let valor = document.getElementById("val").Value;
-  let juros = document.getElementById("ju").Value;
-  let mes = document.getElementById("mes").Value;
+  let valor = document.getElementById("val").value;
+  let juros = document.getElementById("ju").value;
+  let mes = document.getElementById("mes").value;
   let resultado = 0;
   for(let i = 1 <= mes; i++) {
     resultado += valor * (1+(juros/100));
@@ -39,6 +39,27 @@ function total (){
   }
    document.write("O Resultado é" + resultado);
 
+}
+function soma () {
+  let n1 = document.getElementById("n1").value;
+  let n1 = document.getElementById("n2").value;
+  let n1 = document.getElementById("n3").value;
+  let r = Number(n1) + Number(n2) + Number(n3);
+  document.getElementById("resultado").innerHTML = r ;
+}
+function Media () {
+  let n1 = document.getElementById("n1").value;
+  let n1 = document.getElementById("n2").value;
+  let n1 = document.getElementById("n3").value;
+  let r = Number(n1) + Number(n2) + Number(n3) / Number (3);
+  document.getElementById("resultado").innerHTML = r ;
+}
+function falta() {
+  let n1 = document.getElementById("n1").value;
+  let n1 = document.getElementById("n2").value;
+  let n1 = document.getElementById("n3").value;
+  let r = Number (180)-Number(n1) + Number(n2) + Number(n3);
+  document.getElementById("resultado").innerHTML = r ;
 }
 
 
